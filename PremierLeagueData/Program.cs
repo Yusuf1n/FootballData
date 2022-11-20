cb659033aaa495d41c55a7eeb8f4967658e63584
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2022-11-20 18:36:37 +0000

    Added a loop for the main menu as well as a exit option

--- PremierLeagueData/Program.cs
+++ PremierLeagueData/Program.cs
@@ -7,33 +7,41 @@ namespace PremierLeagueData
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Football Data - Main Menu");
-            Console.WriteLine("1. League Standings");
-            Console.WriteLine("2. Fixture Results");
-            Console.WriteLine("3. Top Scorers");
-            Console.WriteLine("4. Top Assistors");
-            Console.WriteLine();
-            Console.Write("Please enter your option between 1-4: ");
-            int option = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
+            bool exit = false;
 
-            switch (option)
+            do
             {
-                case 1:
-                    await ChooseLeague.LeagueforLeagueStandings();
-                    break;
-                case 2:
-                    await ChooseLeague.LeagueAndGWforFixtureResults();
-                    break;
-                case 3:
-                    await ChooseLeague.LeagueforTopScorers();
-                    break;
-                case 4:
-                    await ChooseLeague.LeagueforTopAssistors();
-                    break;
-            }
+                Console.WriteLine("Football Data - Main Menu");
+                Console.WriteLine("1. League Standings");
+                Console.WriteLine("2. Fixture Results");
+                Console.WriteLine("3. Top Scorers");
+                Console.WriteLine("4. Top Assistors");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine();
+                Console.Write("Please enter your option between 1-5: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
 
-            Console.ReadLine();
+                switch (option)
+                {
+                    case 1:
+                        await ChooseLeague.LeagueforLeagueStandings();
+                        break;
+                    case 2:
+                        await ChooseLeague.LeagueAndGWforFixtureResults();
+                        break;
+                    case 3:
+                        await ChooseLeague.LeagueforTopScorers();
+                        break;
+                    case 4:
+                        await ChooseLeague.LeagueforTopAssistors();
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                }
+            }
+            while (!exit);
         }
     }
 }
